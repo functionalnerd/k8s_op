34ce57c938e6eae0635ec247e4e4051993eb17e3
Justin Oliver <joliver@hubspot.com>
2017-09-16 15:28:03 -0400

    Testing

--- k8s_op.go
+++ k8s_op.go
@@ -10,7 +10,6 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/kubernetes"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 /*
 	"sync"
 	"time"
@@ -47,8 +46,6 @@ func NewHdfsOperatorLib() (*HdfsController) {
         fmt.Println("Failed to initialize the Hdfs operator: %s", err)
         return nil
     }
-
-    deletePod(op)
 
 /*
     fmt.Println("Running operator")
@@ -123,31 +120,33 @@ func inClusterConfig() (*rest.Config, error) {
     return nil, nil
 }
 
-func deletePod(op *HdfsController) {
-    podname := "jumbo-test-test-busybox"
-    opts := metav1.NewDeleteOptions(0)
-    opts.PropagationPolicy = &[]metav1.DeletionPropagation{metav1.DeletePropagationForeground}[0]
-
-    if err := op.ClientSet.CoreV1().Pods("hdfs").Delete(podname, opts); err != nil {
-        fmt.Println("failed to delete pod %s", podname)
-    }
-}
-
+/*
+// StatefulSetName returns the expected name of the stateful set for the given spec
+func StatefulSetName(spec *ZookeeperClusterSpec, instance int) string {
+    return fmt.Sprintf("%s-%d", spec.ClusterName, instance)
+}*/
 
 /*
-func restartPod(ctx context.Context, clientset *kubernetes.Clientset, spec *ZookeeperClusterSpec, instance int) error {
+func restartNamenode(clientset *kubernetes.Clientset, instance int) error {
+    podname := getPodName(instance)
+    restartPod(podname)
+}
+
+func getPodName(instance int) string {
+	return fmt.Sprintf("%s-%s%d-0", hdfsClusterName, namenodeName, instance)
+}
+
+func restartPod(clientset *kubernetes.Clientset, podname string) error {
     opts := metav1.NewDeleteOptions(0)
     opts.PropagationPolicy = &[]metav1.DeletionPropagation{metav1.DeletePropagationForeground}[0]
 
-    podname := spec.GetPodName(instance)
-
-    log.INFO(ctx).Printf("Restarting pod %s", podname)
+    fmt.Println("Restarting pod %s", podname)
     startTime, err := getPodStartTimeWithRetries(ctx, clientset, podname, 5)
     if err != nil {
         return errors.Wrap(err, "couldn't get pod start time")
     }
 
-    if err := clientset.CoreV1Client.Pods(NAMESPACE).Delete(podname, opts); err != nil {
+    if err := clientset.CoreV1().Pods(NAMESPACE).Delete(podname, opts); err != nil {
         return fmt.Errorf("failed to delete pod %s", podname)
     }
 
@@ -162,6 +161,71 @@ func restartPod(ctx context.Context, clientset *kubernetes.Clientset, spec *Zook
 
     log.INFO(ctx).Printf("Pod %s started", podname)
     return nil
+}
+
+func getPodStartTime(clientset *kubernetes.Clientset, podname string) (time.Time, error) {
+    pod, err := clientset.CoreV1().Pods(NAMESPACE).Get(podname, metav1.GetOptions{})
+    if err != nil {
+        return time.Time{}, errors.Wrap(err, "failed to get pod")
+    }
+
+    if pod.Status.StartTime == nil {
+        return time.Time{}, errors.New("got nil start time for pod")
+    }
+
+    return pod.Status.StartTime.Time, nil
+}
+
+func getPodStartTimeWithRetries(ctx context.Context, clientset *kubernetes.Clientset, podname string, retries int) (time.Time, error) {
+    for i := 0; i < retries; i++ {
+        startTime, err := getPodStartTime(clientset, podname)
+        if err != nil {
+            log.WARN(ctx).Printf("Failed to get start time for pod %s: %s", podname, err)
+            time.Sleep(5 * time.Second)
+            continue
+        }
+
+        return startTime, nil
+    }
+
+    return time.Time{}, errors.New("failed to get pod start time within the alloted retries")
+}
+
+func waitForNewStartTime(ctx context.Context, clientset *kubernetes.Clientset, podname string, currentStartTime time.Time) error {
+    timeout := time.Now().Add(60 * time.Second)
+    sleepDuration := 5 * time.Second
+    for {
+        if time.Now().After(timeout) {
+            return errors.New("failed to restart within timeout")
+        }
+
+        newStartTime, err := getPodStartTimeWithRetries(ctx, clientset, podname, 5)
+        if err != nil {
+            log.WARN(ctx).Printf("non-fatal error getting pod start time, will retry if we're still within the time out: %s", err)
+            time.Sleep(sleepDuration)
+            continue
+        }
+
+        if newStartTime.After(currentStartTime) {
+            break
+        }
+
+        time.Sleep(sleepDuration)
+    }
+
+    return nil
+}
+*/
+
+/*
+func deletePod() {
+    podname := "jumbo-test-test-busybox"
+    opts := metav1.NewDeleteOptions(0)
+    opts.PropagationPolicy = &[]metav1.DeletionPropagation{metav1.DeletePropagationForeground}[0]
+
+    if err := op.ClientSet.CoreV1().Pods("hdfs").Delete(podname, opts); err != nil {
+        fmt.Println("failed to delete pod %s", podname)
+    }
 }
 */
 
